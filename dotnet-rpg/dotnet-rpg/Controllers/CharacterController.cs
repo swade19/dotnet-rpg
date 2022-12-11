916c830ae05661204bc8bf94c3f967d5fabdd18e
swade19 <sarah.es@hotmail.co.uk>
2022-12-11 12:28:56 +0000

    Created api, added model with  enum for character class.

---
+++ dotnet-rpg/dotnet-rpg/Controllers/CharacterController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    // first derive from controllerBase => mvc class without view support 
+    
+    [ApiController]
+    [Route("api[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character>()
+        {
+            new Character (),
+            new Character
+            {
+                Id = 1,
+                Name = "Sam",
+                Intelligence = 9,
+                Class = RpgClass.Cleric
+            }
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<Character> Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet]
+        public ActionResult<Character> GetSingle()
+        {
+            return Ok(characters[0]);
+        }
+    }
+}
---
+++ dotnet-rpg/dotnet-rpg/Models/RpgClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace dotnet_rpg.Models
+{
+    // j.c allow the enums to appear by keyword not number
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RpgClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3
+    }
+}
